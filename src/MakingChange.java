b5a58de417e1aca986515c6ad3ca7f9a80434ad1
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2025-02-06 23:12:03 -0800

    done

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,30 +7,30 @@
  */
 
 public class MakingChange {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
     public static long countWays(int target, int[] coins) {
+        // Create 2d array table
+        long[][] table = new long[coins.length + 1][target + 1];
 
-        // make a trie starting with the starting coins and branch off with every possible coin branching off of the first level of nodes
-        // then you can mark the leaves as true if the target is hit from the combination of nodes down that path
-        // return false on that leaf if
+        // Base Case: Only 1 way to make 0 amount
+        for (int i = 0; i < coins.length; i++) {
+            table[i][0] = 1;
+        }
 
-        // think about how to make the problem smaller specifically the target and the number of coins
-        // this might allow me to understand how to solve a smaller version of the problem and figure out
-        // how to solve the problem when you expand with more coins or larger target
-
-        // to deal with duplicates we might be able to do something similar to when we worked with horners method and rabinkarp
-        // by footprinting each sequence of coins with a code at the end to understand that they are the same.
-        // you can use the ones place to identify how many coins were used in that place.
-
-
-        //The ideas above were my original ideas to solving the problem but after further consideration with a hint
-        //it seems like it is a good idea to eliminate duplicates and make the problem smaller by branching down whether
-        //you use a certain coin or not there and eliminate it from the set in one of the ways down.
-        //this decreases the number of coins while also decreasing the target so you can make the problem smaller
-        //and improve efficiency, speed, and memory usage.
-        return 0;
+        // Fill the table
+        for (int i = 1; i <= coins.length; i++) {
+            for (int j = 0; j <= target; j++) {
+                // Include the current coin if j is at least as large as the coin's denomination
+                long withCoin = 0;
+                if (j >= coins[i-1]) {
+                    withCoin = table[i][j - coins[i-1]];
+                }
+                // Exclude the current coin
+                long withoutCoin = table[i-1][j];
+                // Sum up the ways including and excluding the current coin
+                table[i][j] = withoutCoin + withCoin;
+            }
+        }
+        // Return the number of ways to make the target amount using the available coins
+        return table[coins.length][target];
     }
 }
